8d1bc83515ce14b0f2f3a769f3357ecd35919b72
krnirmal2 <nirmalkrmajhi14@gmail.com>
2024-02-26 22:00:01 +0530

    Implement Api gate way and route the request of product microservice url to its different local app server using service discovery microservice concept

---
+++ src/main/java/org/example/apigatewayandloadbalancermicroservice/ApiGatewayAndLoadBalancerMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.apigatewayandloadbalancermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayAndLoadBalancerMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayAndLoadBalancerMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/apigatewayandloadbalancermicroservice/ApiGatewayAndLoadBalancerMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apigatewayandloadbalancermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayAndLoadBalancerMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
